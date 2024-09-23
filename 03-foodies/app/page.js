8ed59f4a4ece1c6d58261338dffc266bc514ee81
Luis Najarro <luisnajarro20@gmail.com>
2024-09-23 10:23:40 -0600

    Exercise: Your Task

--- 03-foodies/app/page.js
+++ 03-foodies/app/page.js
@@ -1,9 +1,15 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <main>
       <h1 style={{ color: 'white', textAlign: 'center' }}>
         Time to get started!
       </h1>
+      <Link href="/meals">Meals</Link>
+      <Link href="/meals/share">Share Meals</Link>
+      <Link href="/community">Community</Link>
+      <Link href="/meals/meal-1">Meal 1</Link>
     </main>
   );
 }
