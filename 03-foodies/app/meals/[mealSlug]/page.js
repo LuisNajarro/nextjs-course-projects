d78105590e4b4af63987d32d5867724cb6c76045
Luis Najarro <luisnajarro20@gmail.com>
2024-09-27 11:09:35 -0600

    Adding Dynamic Metadata

--- 03-foodies/app/meals/[mealSlug]/page.js
+++ 03-foodies/app/meals/[mealSlug]/page.js
@@ -1,8 +1,21 @@
-import Image from "next/image";
-import { notFound } from "next/navigation";
+import Image from 'next/image';
+import { notFound } from 'next/navigation';
 
-import classes from "./page.module.css";
-import { getMeal } from "@/lib/meals";
+import classes from './page.module.css';
+import { getMeal } from '@/lib/meals';
+
+export async function generateMetadata({ params }) {
+  const meal = getMeal(params.mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  return {
+    title: meal.title,
+    description: meal.summary,
+  };
+}
 
 export default function MealDetailsPage({ params }) {
   const meal = getMeal(params.mealSlug);
@@ -11,7 +24,7 @@ export default function MealDetailsPage({ params }) {
     notFound();
   }
 
-  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+  meal.instructions = meal.instructions.replace(/\n/g, '<br />');
 
   return (
     <>
